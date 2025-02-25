d6c0eed83b8b5ca2fd187025538723ec579feb35
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-02-25 11:17:25 +0700

    MVC

---
+++ HelloSpringMVC/src/main/java/huy/controller/HelloController.java
@@ -0,0 +1,12 @@
+package huy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping("Say Hi")
+	public String SayHello() {
+		return "chao";
+	}
+}
