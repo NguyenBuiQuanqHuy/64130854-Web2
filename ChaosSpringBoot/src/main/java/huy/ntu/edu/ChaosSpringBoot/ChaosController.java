39d75a8d80a4c0fcd8f3ce7695b8a3fa04da359c
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-03-04 10:49:53 +0700

    Updated

--- ChaosSpringBoot/src/main/java/huy/ntu/edu/ChaosSpringBoot/ChaosController.java
+++ ChaosSpringBoot/src/main/java/huy/ntu/edu/ChaosSpringBoot/ChaosController.java
@@ -1,13 +1,15 @@
 package huy.ntu.edu.ChaosSpringBoot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ChaosController {
-	@RequestMapping("/chao")
-	public String xin_Chao() {
-		//code xu? ly' o? day
+	@GetMapping("/chao")
+	public String xin_Chao(ModelMap m) {
+		m.addAttribute("tb","Dữ liệu thông báo");
 		return "helloView";
 	}
 }
