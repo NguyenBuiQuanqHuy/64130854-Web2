6e9105d65361a57ff36a954d6476ad2f0bf57eee
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-03-14 21:19:30 +0700

    Updated

--- BMI/src/main/java/huy/ntu/edu/BMI/BMIController.java
+++ BMI/src/main/java/huy/ntu/edu/BMI/BMIController.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class BMIController {
-	@RequestMapping(value = "bmi", method = RequestMethod.GET)
+    @RequestMapping(value = "bmi", method = RequestMethod.GET)
     public String showForm() {
         return "bmi"; 
     }
@@ -19,6 +19,7 @@ public class BMIController {
         try {
             double weight = Double.parseDouble(request.getParameter("weight"));
             double height = Double.parseDouble(request.getParameter("height"));
+            String standard = request.getParameter("standard");
             
             if (weight <= 0 || height <= 0) {
                 model.addAttribute("message", "Cân nặng và chiều cao phải lớn hơn 0");
@@ -29,14 +30,26 @@ public class BMIController {
             bmi = Math.round(bmi * 100.0) / 100.0; 
             String result;
             
-            if (bmi < 18.5) {
-                result = "Gầy";
-            } else if (bmi < 24.9) {
-                result = "Bình thường";
-            } else if (bmi < 29.9) {
-                result = "Thừa cân";
-            } else {
-                result = "Béo phì";
+            if ("asia".equals(standard)) {
+                if (bmi < 18.5) {
+                    result = "Gầy";
+                } else if (bmi < 23) {
+                    result = "Bình thường";
+                } else if (bmi < 25) {
+                    result = "Thừa cân";
+                } else {
+                    result = "Béo phì";
+                }
+            } else { 
+                if (bmi < 18.5) {
+                    result = "Gầy";
+                } else if (bmi < 24.9) {
+                    result = "Bình thường";
+                } else if (bmi < 29.9) {
+                    result = "Thừa cân";
+                } else {
+                    result = "Béo phì";
+                }
             }
             
             model.addAttribute("bmi", bmi);
