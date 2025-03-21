fdd843c7ea068fe3fe1eae3f063e11abfd9e9ed3
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-03-21 21:15:02 +0700

    BTVN

---
+++ GiaoDienModunHoa/src/main/java/huy/ntu/edu/GiaoDienModunHoa/controller/HomeController.java
@@ -0,0 +1,17 @@
+package huy.ntu.edu.GiaoDienModunHoa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	  @GetMapping("/")
+	    public String home() {
+	        return "frontEnd/index";
+	    }
+
+	    @GetMapping("/about")
+	    public String about() {
+	        return "frontEnd/about";
+	    }
+}
---
+++ GiaoDienModunHoa/src/test/java/huy/ntu/edu/GiaoDienModunHoa/GiaoDienModunHoaApplicationTests.java
@@ -0,0 +1,13 @@
+package huy.ntu.edu.GiaoDienModunHoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiaoDienModunHoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
