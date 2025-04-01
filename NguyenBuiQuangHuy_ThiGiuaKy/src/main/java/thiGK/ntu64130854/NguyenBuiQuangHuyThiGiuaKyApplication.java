71293f754184b2b0a4daeac67c0cfd0cc17ea22e
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-04-01 09:05:17 +0700

    Tạo dự án

---
+++ NguyenBuiQuangHuy_ThiGiuaKy/src/main/java/thiGK/ntu64130854/NguyenBuiQuangHuyThiGiuaKyApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130854;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenBuiQuangHuyThiGiuaKyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenBuiQuangHuyThiGiuaKyApplication.class, args);
+	}
+
+}
