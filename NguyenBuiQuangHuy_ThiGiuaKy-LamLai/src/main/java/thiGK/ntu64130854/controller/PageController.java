e3130502e5d6ea60d4d13b4ea39a0913e6ed8441
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2025-04-14 21:29:23 +0700

    Updated

--- NguyenBuiQuangHuy_ThiGiuaKy-LamLai/src/main/java/thiGK/ntu64130854/controller/PageController.java
+++ NguyenBuiQuangHuy_ThiGiuaKy-LamLai/src/main/java/thiGK/ntu64130854/controller/PageController.java
@@ -3,6 +3,7 @@ package thiGK.ntu64130854.controller;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import thiGK.ntu64130854.model.Page;
@@ -14,4 +15,24 @@ public class PageController {
 	public String trangchu() {
 		return "index";
 	}
+	
+	public PageController() {
+		// TODO Auto-generated constructor stub
+		 list.add(new Page("1", "Home Page", "home, page", "Welcome to the home page.", "0"));
+	        list.add(new Page("2", "About Page", "about, page", "Information about the website.", "1"));
+	        list.add(new Page("3", "Contact Page", "contact, page", "Get in touch with us.", "1"));
+	        list.add(new Page("4", "Services Page", "services, page", "List of services we offer.", "1"));
+	        list.add(new Page("5", "Blog Page", "blog, page", "Latest blog posts and articles.", "1"));
+	}
+	
+	 @GetMapping("/page/all")
+	    public String getPage(ModelMap model) {
+	        model.addAttribute("pagelist", list);
+	        return "listpage";
+	    }
+	 
+	 @GetMapping("/page/new")
+	    public String addNew(ModelMap model) {
+	        return "newpage";
+	    }
 }
